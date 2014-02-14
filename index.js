f156e348151a0ca00101711e916d9d17df6f0c67
Guillermo Rauch <rauchg@gmail.com>
2014-02-14 09:55:38 -0800

    index: added missing colors and styles

--- index.js
+++ index.js
@@ -48,6 +48,36 @@ function inspect(obj, opts) {
   return formatValue(ctx, obj, ctx.depth);
 }
 
+// http://en.wikipedia.org/wiki/ANSI_escape_code#graphics
+inspect.colors = {
+  'bold' : [1, 22],
+  'italic' : [3, 23],
+  'underline' : [4, 24],
+  'inverse' : [7, 27],
+  'white' : [37, 39],
+  'grey' : [90, 39],
+  'black' : [30, 39],
+  'blue' : [34, 39],
+  'cyan' : [36, 39],
+  'green' : [32, 39],
+  'magenta' : [35, 39],
+  'red' : [31, 39],
+  'yellow' : [33, 39]
+};
+
+// Don't use 'blue' not visible on cmd.exe
+inspect.styles = {
+  'special': 'cyan',
+  'number': 'yellow',
+  'boolean': 'yellow',
+  'undefined': 'grey',
+  'null': 'bold',
+  'string': 'green',
+  'date': 'magenta',
+  // "name": intentionally not styling
+  'regexp': 'red'
+};
+
 function stylizeNoColor(str, styleType) {
   return str;
 }
